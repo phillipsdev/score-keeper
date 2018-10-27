eb0618d7d63adf9e1886cebff8c4143080dc7552
Amanda Phillips <amanda@phillipsdev.org>
2018-10-26 21:45:19 -0500

    working on changing header to a toggle menu for a better responsive view

--- client/src/App.js
+++ client/src/App.js
@@ -20,61 +20,74 @@ import { faInfoCircle } from "@fortawesome/free-solid-svg-icons";
 library.add(faInfoCircle);
 
 class App extends Component {
+  state = {
+    menu: false
+  };
+
+  showMenu = () => {
+    this.setState({ menu: !this.state.menu });
+  };
+
   render() {
     return (
       <div className="App">
-        <ul className="navbar">
-          <li>
-            <NavLink exact to="/" activeClassName="activeNavButton">
-              Home
-            </NavLink>
-          </li>
-          <li>
-            <NavLink to="/canadian_bacon" activeClassName="activeNavButton">
-              Canadian Bacon
-            </NavLink>
-          </li>
-          <li>
-            <NavLink to="/canadian_salad" activeClassName="activeNavButton">
-              Canadian Salad
-            </NavLink>
-          </li>
-          <li>
-            <NavLink to="/canasta" activeClassName="activeNavButton">
-              Canasta
-            </NavLink>
-          </li>
-          <li>
-            <NavLink to="/domino_train" activeClassName="activeNavButton">
-              Domino Train
-            </NavLink>
-          </li>
-          <li>
-            <NavLink to="/farkle" activeClassName="activeNavButton">
-              Farkle
-            </NavLink>
-          </li>
-          <li>
-            <NavLink to="/twisted_farkle" activeClassName="activeNavButton">
-              Twisted Farkle
-            </NavLink>
-          </li>
-          <li>
-            <NavLink to="/hearts" activeClassName="activeNavButton">
-              Hearts
-            </NavLink>
-          </li>
-          <li>
-            <NavLink to="/spades" activeClassName="activeNavButton">
-              Spades
-            </NavLink>
-          </li>
-          <li>
-            <NavLink to="/yahtzee" activeClassName="activeNavButton">
-              Yahtzee
-            </NavLink>
-          </li>
-        </ul>
+        <header className="header">
+          <div className="menu-item" onClick={this.showMenu}>
+            Menu
+          </div>
+          <ul className="navbar">
+            <li>
+              <NavLink exact to="/" activeClassName="activeNavButton">
+                Home
+              </NavLink>
+            </li>
+            <li>
+              <NavLink to="/canadian_bacon" activeClassName="activeNavButton">
+                Canadian Bacon
+              </NavLink>
+            </li>
+            <li>
+              <NavLink to="/canadian_salad" activeClassName="activeNavButton">
+                Canadian Salad
+              </NavLink>
+            </li>
+            <li>
+              <NavLink to="/canasta" activeClassName="activeNavButton">
+                Canasta
+              </NavLink>
+            </li>
+            <li>
+              <NavLink to="/domino_train" activeClassName="activeNavButton">
+                Domino Train
+              </NavLink>
+            </li>
+            <li>
+              <NavLink to="/farkle" activeClassName="activeNavButton">
+                Farkle
+              </NavLink>
+            </li>
+            <li>
+              <NavLink to="/twisted_farkle" activeClassName="activeNavButton">
+                Twisted Farkle
+              </NavLink>
+            </li>
+            <li>
+              <NavLink to="/hearts" activeClassName="activeNavButton">
+                Hearts
+              </NavLink>
+            </li>
+            <li>
+              <NavLink to="/spades" activeClassName="activeNavButton">
+                Spades
+              </NavLink>
+            </li>
+            <li>
+              <NavLink to="/yahtzee" activeClassName="activeNavButton">
+                Yahtzee
+              </NavLink>
+            </li>
+          </ul>
+        </header>
         <Route exact path="/" component={Home} />
         <Route exact path="/canadian_bacon" component={Canadian_Bacon} />
         <Route exact path="/canadian_salad" component={Canadian_Salad} />
