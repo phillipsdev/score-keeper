45963f002b908db4b4ba617f02ed34dc64f075f3
Amanda Phillips <amanda@phillipsdev.org>
2018-10-30 18:34:53 -0500

    add a form element to farkle component, will play with playerCard

---
+++ client/src/components/PlayerCard.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class PlayerCard extends Component {
+  render() {
+    return (
+      <div>
+        <h3>Player Name</h3>
+      </div>
+    );
+  }
+}
+
+export default PlayerCard;
--- client/src/components/farkle/Farkle.js
+++ client/src/components/farkle/Farkle.js
@@ -5,27 +5,53 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 class Farkle extends Component {
   state = {
-    modal: false
+    modal: false,
+    value: ""
   };
 
   toggleModal = () => {
     this.setState({ modal: !this.state.modal });
   };
 
+  handleSubmit = event => {
+    event.preventDefault();
+    this.setState({ value: event.target.value });
+  };
+
   render() {
     return (
-      <div className="game-header">
-        <h1>Farkle</h1>
-        <FontAwesomeIcon
-          icon="info-circle"
-          className="info-icon"
-          onClick={this.toggleModal}
-        />
-        <Modal
-          game="farkle"
-          isOpen={this.state.modal}
-          toggleModal={this.toggleModal}
-        />
+      <div>
+        <div className="game-header">
+          <h1>Farkle</h1>
+          <FontAwesomeIcon
+            icon="info-circle"
+            className="info-icon"
+            onClick={this.toggleModal}
+          />
+          <Modal
+            game="farkle"
+            isOpen={this.state.modal}
+            toggleModal={this.toggleModal}
+          />
+        </div>
+        <div className="player_count_form">
+          <h3>How many players?</h3>
+          <form>
+            <select>
+              <option value="1">1</option>
+              <option value="2">2</option>
+              <option value="3">3</option>
+              <option value="4">4</option>
+              <option value="5">5</option>
+              <option value="6">6</option>
+              <option value="7">7</option>
+              <option value="8">8</option>
+              <option value="9">9</option>
+              <option value="10">10</option>
+            </select>
+            <button onClick={this.handleSubmit}>Go!</button>
+          </form>
+        </div>
       </div>
     );
   }
